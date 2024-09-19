8d0d4f27eb33646ede8fccbff032e893c58ccc88
Adrov Igor <nucleartux@gmail.com>
2024-09-19 16:49:01 +0200

    remove unused react dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import { crx } from '@crxjs/vite-plugin'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
 
-import manifest from './src/manifest'
+import manifest from "./src/manifest";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     build: {
       emptyOutDir: true,
-      outDir: 'build',
+      outDir: "build",
       rollupOptions: {
         output: {
-          chunkFileNames: 'assets/chunk-[hash].js',
+          chunkFileNames: "assets/chunk-[hash].js",
         },
       },
     },
 
-    plugins: [crx({ manifest }), react()],
-  }
-})
+    plugins: [crx({ manifest })],
+  };
+});
