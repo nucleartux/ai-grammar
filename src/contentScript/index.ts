3da356a9fb74aca877c7f8dcda7d52c4b8c05913
Adrov Igor <nucleartux@gmail.com>
2024-09-19 12:14:06 +0200

    remove comment

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -9,8 +9,6 @@ const powerIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24
 
 const loadingIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#cbd5e1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.1 2.182a10 10 0 0 1 3.8 0"/><path d="M13.9 21.818a10 10 0 0 1-3.8 0"/><path d="M17.609 3.721a10 10 0 0 1 2.69 2.7"/><path d="M2.182 13.9a10 10 0 0 1 0-3.8"/><path d="M20.279 17.609a10 10 0 0 1-2.7 2.69"/><path d="M21.818 10.1a10 10 0 0 1 0 3.8"/><path d="M3.721 6.391a10 10 0 0 1 2.7-2.69"/><path d="M6.391 20.279a10 10 0 0 1-2.69-2.7"/></svg>`;
 
-// https://github.com/explainers-by-googlers/prompt-api/blob/b0751b9a4890db979a57787b5302af99dd0a6ef6/README.md
-
 function getPageOffsetTop(elem: HTMLElement | null) {
   let offset = 0;
 
