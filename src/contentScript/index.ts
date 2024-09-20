7d401e65493c8d9a422baa2a5921ed5a84a4a72c
Adrov Igor <nucleartux@gmail.com>
2024-09-20 13:51:21 +0200

    fix error handling

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -14,6 +14,15 @@ const powerIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24
 
 const loadingIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#cbd5e1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.1 2.182a10 10 0 0 1 3.8 0"/><path d="M13.9 21.818a10 10 0 0 1-3.8 0"/><path d="M17.609 3.721a10 10 0 0 1 2.69 2.7"/><path d="M2.182 13.9a10 10 0 0 1 0-3.8"/><path d="M20.279 17.609a10 10 0 0 1-2.7 2.69"/><path d="M21.818 10.1a10 10 0 0 1 0 3.8"/><path d="M3.721 6.391a10 10 0 0 1 2.7-2.69"/><path d="M6.391 20.279a10 10 0 0 1-2.69-2.7"/></svg>`;
 
+type Result<T> = { ok: true; value: T } | { ok: false; error: unknown };
+
+const resultFromPromise = <T>(promise: Promise<T>): Promise<Result<T>> => {
+  return promise.then(
+    (value) => ({ ok: true, value }),
+    (error) => ({ ok: false, error }),
+  );
+};
+
 function getPageOffsetTop(elem: HTMLElement | null) {
   let offset = 0;
 
@@ -143,7 +152,7 @@ class OllamaProvider implements Provider {
     });
 
     if (!response) {
-      throw new Error("Something went wrong. Please try again.");
+      throw new Error("Make sure that Ollama is installed and running.");
     }
 
     return response.response;
@@ -356,22 +365,24 @@ class Control {
     this.#button.innerHTML = loadingIcon;
     this.#tooltip.text = "Loading...";
 
-    try {
-      const result = await this.#provider.fixGrammar(text);
-      if (this.#text !== text) {
-        return;
-      }
-      this.#result = result;
+    const result = await resultFromPromise(this.#provider.fixGrammar(text));
+    if (this.#text !== text) {
+      return;
+    }
+
+    if (result.ok) {
+      this.#result = result.value;
 
       this.#button.innerHTML = this.#isCorrect ? checkIcon : infoIcon;
       if (this.#isCorrect) {
         this.#hideTooltip();
       }
 
-      this.#tooltip.text = createDiff(text, result);
-    } catch (e: any) {
-      console.warn(e);
-      const message = e?.message ?? e?.toString();
+      this.#tooltip.text = createDiff(text, result.value);
+    } else {
+      const error = result.error as any;
+      console.warn(error);
+      const message = error?.message ?? error?.toString();
       this.#tooltip.text =
         "Something went wrong. Please try again." +
         (message ? ` (${message})` : "");
