e687e5df90d2001787d213633e369da9ac2ac85b
Adrov Igor <nucleartux@gmail.com>
2024-09-24 11:45:13 +0200

    add retry on model load

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,11 +1,34 @@
-import ollama, { Ollama } from "ollama/browser";
+import ollama, { GenerateResponse, Ollama } from "ollama/browser";
 
 let abortController = new AbortController();
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const fetchWithSignal = (...args: Parameters<typeof fetch>) => {
   return fetch(args[0], {
     ...args[1],
     signal: abortController.signal,
+  });
+};
+
+const generate = (
+  args: Parameters<typeof ollama.generate>[0],
+): Promise<GenerateResponse> => {
+  const ollama = new Ollama({
+    fetch: fetchWithSignal,
+  });
+  return ollama.generate(args).catch((e) => {
+    console.warn(e);
+    const message: string | undefined = (e as any)?.message;
+    if (message === "unexpected server status: llm server loading model") {
+      return sleep(3000).then(() => {
+        if (abortController.signal.aborted) {
+          throw new Error("Aborted");
+        }
+        return generate(args);
+      });
+    }
+    throw e;
   });
 };
 
@@ -20,16 +43,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "ollama.generate") {
     abortController.abort();
     abortController = new AbortController();
-    const ollama = new Ollama({
-      fetch: fetchWithSignal,
-    });
-    ollama
-      .generate(request.data)
+    generate(request.data)
       .then((result) => sendResponse(result))
-      .catch((e) => {
-        console.warn(e);
-        return sendResponse(null);
-      });
+      .catch(() => sendResponse(null));
+
     return true;
   }
 });
